cadeca230470b82e6221cb7e4151585a96f50e3f
Qasim Ali <qasimali24@gmail.com>
2018-11-07 19:42:31 +0500

    Added refrences.

--- server.js
+++ server.js
@@ -18,8 +18,20 @@ app.get('/',(req,res) => {
   res.render('dumy2/index.hbs');
 })
 
-app.get('/dumy',(req,res) => {
-  res.render('dumy.hbs');
+app.get('/eft',(req,res) => {
+  res.render('dumy2/EFT.hbs');
+})
+
+app.get('/distancehealing',(req,res) => {
+  res.render('dumy2/distancehealing.hbs');
+})
+
+app.get('/umore',(req,res) => {
+  res.render('dumy2/umore.hbs');
+})
+
+app.get('/aboutme',(req,res) => {
+  res.render('dumy2/aboutme.hbs');
 })
 
 app.listen(port, () => {
